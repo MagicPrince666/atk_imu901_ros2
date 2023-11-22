9934adc8d6ae2d8d872aaa6a10045f8a844858ce
MagicPrince666 <846863428@qq.com>
2023-11-22 21:48:13 +0800

    支持旧版本ros2

--- launch/imu901m.launch.py
+++ launch/imu901m.launch.py
@@ -2,7 +2,6 @@ import os
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
 
 from launch_ros.actions import LifecycleNode
@@ -15,7 +14,16 @@ def generate_launch_description():
 
     imu_cfg = os.path.join(get_package_share_directory('ros2_imu'), 'params', 'imu_cfg.yaml')
 
-    return LaunchDescription([
-        Node(namespace='/', package='ros2_imu', executable='ros2_imu_node', name='ros2_imu_node', parameters=[imu_cfg], output='screen'),
-        Node(package='tf2_ros', executable='static_transform_publisher', arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','imu_link']),
-    ])
+    ROS_DISTRO=''
+    ROS_DISTRO = os.getenv('ROS_DISTRO')
+    print("Current ROS2 Version: ",ROS_DISTRO)
+    if ROS_DISTRO == 'humble' and ROS_DISTRO == 'foxy':
+        return LaunchDescription([
+            Node(namespace='/', package='ros2_imu', executable='ros2_imu_node', name='ros2_imu_node', parameters=[imu_cfg], output='screen'),
+            Node(package='tf2_ros', executable='static_transform_publisher', arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','imu_link']),
+        ])
+    else:
+        return LaunchDescription([
+            Node(node_namespace='/', package='ros2_imu', node_executable='ros2_imu_node', node_name='ros2_imu_node', parameters=[imu_cfg], output='screen'),
+            Node(package='tf2_ros', node_executable='static_transform_publisher', arguments=['0', '0', '0.02','0', '0', '0', '1','base_link','imu_link']),
+        ])
