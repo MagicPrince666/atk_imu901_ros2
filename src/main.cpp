18e0a70ae890bafa1d871cc0f56cfb88899c80dd
MagicPrince666 <846863428@qq.com>
2023-12-22 21:15:11 +0800

    add spin

--- src/main.cpp
+++ src/main.cpp
@@ -82,6 +82,7 @@ int main(int argc, char *argv[])
     rclcpp::init(argc, argv);
     auto node = std::make_shared<rclcpp::Node>("imu901m");
     std::unique_ptr<ImuPub> imu_mode(new ImuPub(node));
+    rclcpp::spin(node);
     rclcpp::shutdown();
 #endif
 
